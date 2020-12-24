a81a5b368ee8b0965fb5541788febd381686ed43
chucvv <chucvv@vng.com.vn>
2020-12-24 17:38:57 +0700

    [DB] Setup postgis

--- infrastructure/server-config/src/vn/com/libertime/serverconfig/Env.kt
+++ infrastructure/server-config/src/vn/com/libertime/serverconfig/Env.kt
@@ -1,6 +1,3 @@
 package vn.com.libertime.serverconfig
 
 internal const val defaultEnvironment = "sandbox"
-internal const val productionEnvironment = "production"
-
-internal fun isProduction(environment: String): Boolean = environment == productionEnvironment
